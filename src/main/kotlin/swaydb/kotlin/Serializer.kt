af5d1a78266d1c3660d6a3657e1068adb1fbba6a
Valentyn Kolesnikov <javadev75@gmail.com>
2019-04-28 19:51:30 +0300

    Add travis integration.

--- src/main/kotlin/swaydb/kotlin/Serializer.kt
+++ src/main/kotlin/swaydb/kotlin/Serializer.kt
@@ -16,7 +16,7 @@
 * You should have received a copy of the GNU Affero General Public License
 * along with SwayDB. If not, see <https://www.gnu.org/licenses/>.
 */
-package swaydb.kotlin;
+package swaydb.kotlin
 
 import java.util.HashMap
 import swaydb.serializers.Default
