8afdfbad1346022cee211f3b7cfcf275a806cc0c
simerplaha <simer.j@gmail.com>
2020-03-19 12:09:48 +1100

    QuickStartMapSimple

---
+++ src/main/kotlin/quickstart/QuickStartMapSimple.kt
@@ -0,0 +1,42 @@
+package quickstart
+
+import swaydb.KeyVal
+import swaydb.java.Stream
+import swaydb.java.memory.MapConfig
+import swaydb.java.serializers.Default
+import java.time.Duration
+
+internal object QuickStartMapSimple {
+
+  @JvmStatic
+  fun main(args: Array<String>) {
+    val map =
+      MapConfig
+        .functionsOff(Default.intSerializer(), Default.intSerializer())
+        .get()
+
+    map.put(1, 1) //basic put
+    map[1].get() //basic get
+    map.expire(1, Duration.ofSeconds(1)) //basic expire
+    map.remove(1) //basic remove
+
+    //atomic write a Stream of key-value
+    map.put(Stream.range(1, 100).map { KeyVal.create(it) })
+
+    //Create a stream that updates all values within range 10 to 90.
+    val updatedKeyValues =
+      map
+        .from(10)
+        .stream()
+        .takeWhile { it.key() <= 90 }
+        .map { KeyVal.create(it.key(), it.value() + 5000000) }
+
+    //submit the stream to update the key-values as a single transaction.
+    map.put(updatedKeyValues)
+
+    //print all key-values to view the update.
+    map
+      .stream()
+      .forEach(::println)
+  }
+}
