89f9bedbec2054d86d1ce4d57b6d6fe6e4792f2c
simerplaha <simer.j@gmail.com>
2020-03-19 12:04:11 +1100

    ReverseOrderingTest

---
+++ src/main/kotlin/ordering/ReverseOrderingTest.kt
@@ -0,0 +1,32 @@
+package ordering
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import swaydb.java.memory.MapConfig
+import swaydb.java.serializers.Default
+
+internal class ReverseOrderingTest {
+  @Test
+  fun reverse() {
+    val map =
+      MapConfig.functionsOff(Default.intSerializer(), Default.intSerializer()) //provide a typed comparator that reverses ordering
+        .setTypedComparator { key1: Int, key2: Int? -> key1.compareTo(key2!!) * -1 }
+        .get()
+
+    //insert in natural ordering from 1 to 100
+    (1..100).forEach { map.put(it, it) }
+
+    val actual =
+      map
+        .keys()
+        .stream()
+        .materialize()
+
+    //print out the stream. Since ordering is in reverse this will print from 100 to 1.
+    actual.forEach(::println)
+
+    val expected = (1..100).map { 100 - it + 1 }
+
+    assertEquals(expected, actual)
+  }
+}
--- src/main/kotlin/quickstart/QuickStart.kt
+++ src/main/kotlin/quickstart/QuickStart.kt
@@ -9,18 +9,19 @@ import java.time.Duration
 object QuickStart {
 
   //some function with random logic that we want to use in our map
-  val function = PureFunction.OnKeyValue<Int, Int, Return.Map<Int>> { key, value, _ ->
-    when {
-      key < 25 -> //remove if key is less than 25
-        Return.remove()
-      key < 50 -> //expire after 2 seconds if key is less than 50
-        Return.expire(Duration.ofSeconds(2))
-      key < 75 -> //update if key is < 75.
-        Return.update(value!! + 10000000)
-      else -> //else do nothing
-        Return.nothing()
+  private val function =
+    PureFunction.OnKeyValue<Int, Int, Return.Map<Int>> { key, value, _ ->
+      when {
+        key < 25 -> //remove if key is less than 25
+          Return.remove()
+        key < 50 -> //expire after 2 seconds if key is less than 50
+          Return.expire(Duration.ofSeconds(2))
+        key < 75 -> //update if key is < 75.
+          Return.update(value!! + 10000000)
+        else -> //else do nothing
+          Return.nothing()
+      }
     }
-  }
 
   @JvmStatic
   fun main(args: Array<String>) {
@@ -37,15 +38,15 @@ object QuickStart {
     map.remove(1) //basic remove
 
     //atomic write a Stream of key-value
-    map.put(Stream.range(1, 100).map { item -> KeyVal.create(item) })
+    map.put(Stream.range(1, 100).map { KeyVal.create(it) })
 
     //create a read stream from 10th key-value to 90th, increment values by 1000000 and insert.
     val updatedKeyValuesStream =
       map
         .from(10)
         .stream()
-        .takeWhile { keyVal -> keyVal.key() <= 90 }
-        .map { keyVal -> KeyVal.create(keyVal.key(), keyVal.value() + 5000000) }
+        .takeWhile { it.key() <= 90 }
+        .map { KeyVal.create(it.key(), it.value() + 5000000) }
 
     map.put(updatedKeyValuesStream)
 
@@ -54,6 +55,6 @@ object QuickStart {
     //print all key-values to view the update.
     map
       .stream()
-      .forEach { println(it) }
+      .forEach(::println)
   }
 }
